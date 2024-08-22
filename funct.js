3519caf569430335984122a899d912a419cbff71
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-08-22 17:18:31 +0500

    normal function & local variables

---
+++ funct.js
@@ -0,0 +1,37 @@
+// function myFunction(){
+//     console.log("welcome to js");
+//     console.log("we are learning js");
+// }
+// myFunction();
+
+// function myFunction(msg){   //parameter => input
+    
+//     console.log(msg)
+// }
+// myFunction("i am umair"); //argument  
+
+
+// function mult(num1,num2){
+//     console.log(num1 * num2);
+// }
+// mult(5,4);
+
+//return
+// function mult(num1,num2){
+// res = num1 * num2;
+// return res;
+// }
+//  let val = mult(5,4);
+//  console.log(val);
+
+
+//parameter are local variables of functions
+// function mult(num1,num2){
+//     console.log(num1 * num2);
+//     console.log(num1); //it works
+// }
+// mult(5,4);
+// console.log(num1);  //it do not work
+
+
+
