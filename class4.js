4665a132a7b45dd66f5a713fe705a62ecd632774
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-08-27 18:07:00 +0500

    for each loop

---
+++ class4.js
@@ -0,0 +1,32 @@
+//FOR EACH LOOP in ARRAY
+
+//SYNTAX
+// array.forEach(function(element, index, array) {
+    
+//     // Code to execute for each element
+//   });
+
+
+//Simple Example
+// const fruits = ["banana","apple","Pineapple","cherry"];
+// fruits.forEach(function val(fruit){
+//     console.log(fruit);
+// })
+
+//Using Arrow Functions
+// const fruits = ["banana","apple","Pineapple","cherry"];
+// fruits.forEach((fruit)=>{
+//     console.log(fruit);
+// })
+
+//touppercase
+// const fruits = ["banana","apple","Pineapple","cherry"];
+// fruits.forEach((fruit)=>{
+//     console.log(fruit.toUpperCase());
+// })
+
+//CALL-BACK-FUNCTION K ANDAR THREEE PARAMETER HOTA HE
+// const fruits = ["banana","apple","Pineapple","cherry"];
+// fruits.forEach((fruit,index,fruits)=>{
+//     console.log(fruit.toUpperCase(),index,fruits);
+// })
